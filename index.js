d620d2d67b6699bfb8f21dea96c3a92aa1f4e191
Shahid Amin <96911219+aminshahid573@users.noreply.github.com>
2025-02-17 16:16:43 +0000

    simple Server made

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+//import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res)=>{
+    res.send("Twitter")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
